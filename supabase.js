d14e297f8226b9fdc561e2fd6595719e04bf721e
Coder5330 <261540h@student.hci.edu.sg>
2026-05-05 18:37:41 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
 // shared supabase client — include this before any other script that needs it
-const SUPABASE_URL = 'https://ssdspxaqxszhtnatzwso.supabase.co';
+const SUPABASE_URL = 'https://kfwdknxtwimlqypitwwf.supabase.co';
 const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtmd2Rrbnh0d2ltbHF5cGl0d3dmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5NzMzNTIsImV4cCI6MjA5MzU0OTM1Mn0.sjliRGHAmcU2K-T7xovZjxVCEvRDFc6IAapKaaQqDXY';
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON);
